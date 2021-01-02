1e223bae1e2f084d711b7b9ff656df7d225207d4
Kyle Weisse <kyle.weisse@gmail.com>
2021-01-02 12:29:56 -0500

    jquery targeting and scss use

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,11 @@ $(function() {
     });
 
     
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
+    });
+
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show');
+    });
 });
